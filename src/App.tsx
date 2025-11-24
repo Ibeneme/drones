9f3726837f795c5d355377888fa7bbc4af54abf9
ibeneme <ikennaibenemee@gmail.com>
2025-11-24 07:58:07 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { useTheme } from "./contexts/ThemeProvider";
+
 import BoundariesPage from "./components/BoundariesPage";
 import DroneProductSection from "./components/DroneProductSection";
 import ExplorePage from "./components/ExplorePage";
@@ -8,22 +10,33 @@ import HeroSection from "./components/HeroSection";
 import UnleashDrone from "./components/UnleashDrone";
 
 function App() {
+  const { theme } = useTheme();
+  const isDark = theme === "dark";
+
   return (
-    <>
-      {/* FULL PAGE WRAPPER to prevent overflow */}
+    <div
+      className={`
+        min-h-screen w-full 
+        transition-colors duration-500 
+        ${isDark ? "bg-black text-white" : "bg-white text-gray-900"}
+      `}
+    >
       <div className="max-w-7xl mx-auto w-full overflow-x-hidden px-4">
+        {/* Navbars */}
         <GuardDroneNavbar />
         <GlassNavbar />
 
+        {/* Sections */}
         <HeroSection />
-        <DroneProductSection />
+       	<DroneProductSection />
         <ExplorePage />
         <BoundariesPage />
         <UnleashDrone />
 
+        {/* Footer */}
         <Footer />
       </div>
-    </>
+    </div>
   );
 }
 
